866ccdd2008e356638d6d4203026ed95d1d92060
Isabella Santos <isabella.santos@gocase.com.br>
2017-04-27 18:27:23 -0300

    Check profile options

--- app/models/spree/gateway/paypal_web_profile.rb
+++ app/models/spree/gateway/paypal_web_profile.rb
@@ -28,10 +28,14 @@ module Spree
     def first_or_new(options)
       list = get_list
       if list.empty?
-        web_profile_class.new(request_hash(options))
+        selected_profile = web_profile_class.new(request_hash(options))
       else
-        list.first
+        selected_profile = list.find { |item| have_correct_options?(item, options) }
+        if selected_profile.nil?
+          selected_profile = web_profile_class.new(request_hash(options))
+        end
       end
+      selected_profile
     end
 
     def request_hash(options)
@@ -43,7 +47,7 @@ module Spree
         },
         input_fields:{
           allow_note: options[:allow_note],
-          no_shipping: options[:no_shipping],
+          no_shipping: format_shipping_config(options[:no_shipping]),
           address_override: options[:address_override],
         },
         flow_config:{
@@ -51,6 +55,17 @@ module Spree
           return_uri_http_method: 'get'
         }
       }
+    end
+
+    def have_correct_options?(profile, options)
+      return false if profile.presentation.try(:brand_name).to_s != options[:brand_name]
+      return false if profile.presentation.try(:locale_code) != options[:locale_code]
+      return false if profile.input_fields.try(:no_shipping) != format_shipping_config(options[:no_shipping])
+      return false if profile.input_fields.try(:address_override) != options[:address_override].to_i
+    end
+
+    def format_shipping_config(value)
+      value.present? ? '1' : '2'
     end
 
   end
