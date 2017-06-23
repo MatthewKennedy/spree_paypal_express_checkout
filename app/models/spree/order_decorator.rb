99caa37479eaf6117beafb674ab947a7fc05c380
Isabella Santos <isabella.santos@gocase.com.br>
2017-06-23 15:34:52 -0300

    Fix rate duplicity

--- app/models/spree/order_decorator.rb
+++ app/models/spree/order_decorator.rb
@@ -10,7 +10,14 @@ module Spree
         package = shipment.to_package
         shipping_methods = shipment.available_paypal_shipping_methods
         rates = shipment.paypal_shipping_rates
-        rates.each { |rate| rate.save }
+        rates.each do |rate|
+          old_rates = Spree::ShippingRate.where(shipment_id: rate.shipment_id,
+                                                shipping_method_id: rate.shipping_method_id)
+          old_rates.each do |old_rate|
+            old_rate.destroy if old_rate.order.try(:id) == id
+          end if old_rates.any?
+          rate.save
+        end
       end
       set_shipments_cost
       apply_free_shipping_promotions
